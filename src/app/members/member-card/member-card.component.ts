805a9f2de627c6f111879dea36437ec10f3b1acb
thaovv <vuvanthao2408.nd@gmail.com>
2024-03-27 23:32:35 +0700

    end 9

--- src/app/members/member-card/member-card.component.ts
+++ src/app/members/member-card/member-card.component.ts
@@ -1,16 +1,10 @@
-import { Component,Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Member } from '../../_models/menber';
-
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrls: ['./member-card.component.css']
 })
 export class MemberCardComponent {
-    @Input() member: Member | undefined;
-    constructor(){}
-    ngOnInit(): void {
-      
-    }
-
+  @Input() member: Member | undefined;
 }
--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Member } from '../../_models/menber';
 import { MembersService } from '../../_services/members.service';
 
-
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
